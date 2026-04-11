ded7020a6ab736b62f42033063c18226e82b7291
Staaaaaaaaar <2300012435@stu.pku.edu.cn>
2026-04-11 23:21:31 +0800

    ✨ feat: Implement a CNN

--- assignment-02/cifar-10/network.py
+++ assignment-02/cifar-10/network.py
@@ -4,23 +4,55 @@ import torch.nn as nn
 class ConvNet(nn.Module):
     def __init__(self, num_class=10):
         super(ConvNet, self).__init__()
-        pass
         # ----------TODO------------
         # define a network 
         # ----------TODO------------
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.15),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128 * 4 * 4, 256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(256, num_class),
+        )
 
     def forward(self, x):
 
         # ----------TODO------------
         # network forwarding 
         # ----------TODO------------
+        x = self.features(x)
+        x = self.classifier(x)
 
         return x
 
 
 if __name__ == '__main__':
     import torch
-    from torch.utils.tensorboard  import SummaryWriter
+    from torch.utils.tensorboard import SummaryWriter
     from dataset import CIFAR10
     writer = SummaryWriter(log_dir='../experiments/network_structure')
     net = ConvNet()
